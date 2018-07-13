1259dd344091fd3477620cd020c9d6f86090c937
Hayden Le <haydlite@users.noreply.github.com>
2018-07-12 21:55:37 -0500

    pickle tweets

--- tweets_collection.py
+++ tweets_collection.py
@@ -1,14 +1,21 @@
+import datetime as dt
+import pickle
+
 from twitterscraper import query_tweets
 
+
 if __name__ == '__main__':
-    list_of_tweets = query_tweets("Trump OR Clinton", 10)
+    #list_of_tweets = query_tweets(("James Franco"), limit=None, begindate=dt.date(2017, 6, 1), enddate=dt.date(2017, 6, 2),
+    #                              poolsize=20, lang='en');
 
-    #print the retrieved tweets to the screen:
-    for tweet in query_tweets("Trump OR Clinton", 10):
-        print(tweet)
 
-    #Or save the retrieved tweets to file:
-    file = open("output.txt","w")
-    for tweet in query_tweets("Trump OR Clinton", 10):
-        file.write(tweet.encode('utf-8'))
-    file.close()
+    #with open('james_franco.pkl', 'wb') as outfile:
+    #    pickle.dump(list_of_tweets, outfile)
+
+    with open('james_franco.pkl', 'rb') as infile:
+        tweets = pickle.load(infile)
+
+    for tweet in tweets:
+        print(tweet.retweets + tweet.text)
+
+    # TODO: Sentiment analysis.
