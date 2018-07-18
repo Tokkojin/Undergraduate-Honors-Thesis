5a16b8caa4aeb4932b7a9149d96f7a9ef51983b8
haydlite <thaiype@gmail.com>
2018-07-17 23:01:05 -0500

    ordered tweets by date; trying to extract a subset for initial review

---
+++ data_visual.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+from matplotlib import pyplot
+from datetime import datetime, date
+
+tweets = pd.read_json('james_franco_lex_pol.json')
+tweets.sort_values(by='timestamp', inplace=True)
+tweets.where(tweets['timestamp'])
+
+startDate = datetime(2017, 12, 1)
+endDate = datetime(2018, 2, 15)
+
+def to_datetime(x):
+    return datetime.strptime(str(x), '%Y-%m-%d %X')
+
+tweets['timestamp'].apply(lambda x: to_datetime(x))
+
+tweets_stub = tweets(tweets['timestamp'] >= startDate and tweets['timestamp'] <= endDate)
+
+# look into https://stackoverflow.com/questions/30689445/datetime64-comparison-in-dataframes
