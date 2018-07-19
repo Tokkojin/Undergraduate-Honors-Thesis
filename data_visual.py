ec301bb6e5152b3d4e36c2567b1d2606a2cfd128
Hayden Le <haydlite@users.noreply.github.com>
2018-07-19 01:20:03 -0500

    exploratory seaborn graph

--- data_visual.py
+++ data_visual.py
@@ -1,21 +1,27 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
-import seaborn; seaborn.set()
 from datetime import date
+
+sns.set()
 
 tweets = pd.read_json('james_franco_lex_pol.json')
 tweets.sort_values(by='timestamp', inplace=True)
-tweets.where(tweets['timestamp'])
-
 startDate = date(2017, 12, 15)
 endDate = date(2018, 2, 15)
 
-# https://stackoverflow.com/questions/41783003/how-do-i-convert-timestamp-to-datetime-date-in-pandas-dataframe
 tweets['timestamp'] = pd.to_datetime(tweets['timestamp']).apply(lambda x: x.date())
 
 tweets_stub = tweets[tweets['timestamp'] >= startDate]
 tweets_stub = tweets_stub[tweets_stub['timestamp'] <= endDate]
 tweets_stub.sort_values(by='timestamp', inplace=True)
 
-tweets_date_grouped = tweets_stub.groupby(by=['timestamp','lexicon_polarity']).count()['text']
+df = tweets_stub.groupby(['timestamp', 'lexicon_polarity']).size()
+df = df.reset_index()
+df.columns = ['timestamp', 'polarity', 'size']
+# df = df.pivot(index='timestamp', columns='polarity', values='size')
+ax = sns.lineplot(x='timestamp', y='size', hue='polarity', palette='pastel', data=df)
+ax.set_xlabel = "Date"
+ax.set_ylabel = "Number of Tweets"
+plt.show()
