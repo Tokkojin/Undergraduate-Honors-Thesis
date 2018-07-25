3654df2e2beabe28b8e7f000f3ca05efcff44142
Hayden Le <haydlite@users.noreply.github.com>
2018-07-24 23:11:52 -0500

    pull links + info about them -> csv file

--- tweets_links.py
+++ tweets_links.py
@@ -1,56 +1,30 @@
-import pandas as pd
-from bs4 import BeautifulSoup
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
+import re
+import csv
 
-from multiprocessing import cpu_count, Pool
-
-from tqdm import tqdm
-
-import pickle
-
+import pandas as pd
 
 def find_URL(row):
-    soup = BeautifulSoup(row['html'], 'html.parser')
-
-    links = []
-
-    for link in soup.find_all('a'):
-        links.append(link)
-        print(link.get('href'))
-
-    return links
-
-def process_data(func, df, num_processes=None):
-    if num_processes == None:
-        num_processes = min(df.shape[0], cpu_count())
-
-    with Pool(processes = num_processes) as pool:
-        rows = []
-
-        for index, row in df.iterrows():
-            rows.append(row)
-
-        URLs = list(tqdm(pool.imap(find_URL, rows), total=len(df.index)))
-
-    return URLs
-
+    URLs = re.findall(r'(https?://[^\s]+)', row['text'])
+    
+    for URL in URLs:
+        csvwriter.writerows( [[str(row['timestamp']), URL, row['likes'], row['replies'], row['retweets']]] )
 
 if __name__ == '__main__':
     infile = sys.argv[1] + '.json'
-    outfile = sys.argv[1] + '.pkl'
+    outfile = sys.argv[1] + '.csv'
 
-    print('reading from ' + infile)
-    corpus = pd.read_json(infile)
-
-    print('finding URLs...')
-    URLs = process_data(find_URL, corpus, num_processes=cpu_count())
-
-    print('saving to ' + outfile)
-
-    with open(outfile, 'wb') as fp:
-        pickle.dump(URLs, fp)
-
-    # To load from .pkl...
-    # with open('outfile', 'rb') as fp:
-    #     itemlist = pickle.load(fp)
+    with open(infile, 'rb') as fp:
+        corpus = pd.read_json(infile)
+    
+    f = open(outfile, 'w', newline='')
+    csvwriter = csv.writer(f)
+    csvwriter.writerows( [['timestamp', 'URL', 'likes', 'replies', 'retweets']] )
+    
+    corpus.apply(find_URL, axis=1)
+    
+    f.close()
+    
