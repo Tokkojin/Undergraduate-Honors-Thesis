5db6c2e77f9121f9b80199ff9b5b369a16e0e20b
Hayden Le <haydlite@users.noreply.github.com>
2018-07-29 19:27:04 -0500

    body text cleanup

--- follow_links.py
+++ follow_links.py
@@ -7,6 +7,23 @@ from urllib.error import URLError, HTTPError
 
 import pandas as pd
 from bs4 import BeautifulSoup
+from bs4.element import Comment
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)  
+    return u" ".join(t.strip() for t in visible_texts)
 
 if __name__ == '__main__':
     infile = sys.argv[1] + '.csv'
@@ -15,13 +32,24 @@ if __name__ == '__main__':
     
     links = dict(zip(df.id, df.URL))
     
+    # This uses the 3-argument version of str.maketrans
+    # with arguments (x, y, z) where 'x' and 'y'
+    # must be equal-length strings and characters in 'x'
+    # are replaced by characters in 'y'. 'z'
+    # is a string (string.punctuation here)
+    # where each character in the string is mapped
+    # to None
+    translator = str.maketrans('', '', string.punctuation)
+    
+    # bow = CountVectorizer()
+    
     for link in links.values():
-        link = link.translate(None, string.punctuation)
-        if(link.find('youtube') == -1):
+
+        link_clean = link.translate(translator)
+        if(link_clean.find('youtube') == -1 and link_clean.find('twitter') == -1):
             try:
                 html_page = urlopen(link)
-                soup = BeautifulSoup(html_page)
-                body = soup.find_all("p")
+                body = text_from_html(html_page)
                 if(len(body) > 0):
                     print(body)
             except HTTPError as e:
